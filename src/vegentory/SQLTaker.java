31883fd74e22afc75146921cba713d2eddea3ad2
Skiadra <riadywiguna5@gmail.com>
2024-02-25 20:36:27 +0700

    Init
    Move project to github

---
+++ src/vegentory/SQLTaker.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package vegentory;
+
+/**
+ *
+ * @author riady
+ */
+public class SQLTaker {
+
+    public static String instanceName = "localhost\\LUKSERVER;";
+    public static String dbName = "Vegentory;";
+    public static String id = "sa;";
+    public static String pass = "kopi12345;";
+
+}
